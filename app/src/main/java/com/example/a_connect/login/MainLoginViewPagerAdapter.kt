b3e47481e4fba1e998e4f67c4618172b1f97a550
prabal <prabal1513@gmail.com>
2024-11-08 20:07:43 +0530

    Login UI

---
+++ app/src/main/java/com/example/a_connect/login/MainLoginViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.a_connect.login
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainLoginViewPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+
+        return when(position){
+            0-> AluminiLogin()
+            else-> StudentLogin()
+
+        }
+    }
+}
