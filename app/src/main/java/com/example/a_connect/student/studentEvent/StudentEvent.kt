0a9bcc58aa3315dc85c6a66719aeff286214f552
AnuragGupta4700 <anuraggupta4700@gmail.com>
2024-11-30 01:16:35 +0530

    student event main screen created

--- app/src/main/java/com/example/a_connect/student/studentEvent/StudentEvent.kt
+++ app/src/main/java/com/example/a_connect/student/studentEvent/StudentEvent.kt
@@ -5,56 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.a_connect.R
+import com.example.a_connect.databinding.FragmentStudentEventBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentEvent.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentEvent : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentStudentEventBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_event, container, false)
+        _binding = FragmentStudentEventBinding.inflate(inflater, container, false)
+
+        // Initialize view pager Adapter
+        val viewpageradapter = StudentEventPagerAdapter(this)
+        binding.viewPager.adapter = viewpageradapter
+
+        // Tab titles
+        val tabTitles = listOf("Upcoming Events", "Past Events")
+
+        // Attach TabLayout with ViewPager2
+        TabLayoutMediator(binding.eventTabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
+        return binding.root // Return the binding root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentEvent.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentEvent().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
---
+++ app/src/main/java/com/example/a_connect/student/studentEvent/StudentEventDataItem.kt
@@ -0,0 +1,10 @@
+package com.example.a_connect.student.studentEvent
+
+class StudentEventDataItem(
+    val imageResId: Int,
+    val eventTitle: String,
+    val eventDate: String,
+    val eventTime: String,
+    val bookmark: Int
+)
+
