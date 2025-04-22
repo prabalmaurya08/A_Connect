7f845ca3bef18dfaa3b5f1d6158562afae6e58a4
ishan Parashar <Ishanparashar24@gmail.com>
2025-04-22 18:15:53 +0530

    error fixes

--- app/src/main/java/com/example/a_connect/login/AdminLogin.kt
+++ app/src/main/java/com/example/a_connect/login/AdminLogin.kt
@@ -1,15 +1,13 @@
 package com.example.a_connect.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.a_connect.R
 import com.example.a_connect.databinding.FragmentAdminLoginBinding
-import com.google.android.material.appbar.AppBarLayout
 
 class AdminLogin : Fragment() {
 
@@ -23,8 +21,6 @@ class AdminLogin : Fragment() {
         binding=FragmentAdminLoginBinding.inflate(layoutInflater)
 
         //toolbar implementation in appBarLayout in admin login fragment
-        val appBarLayout: AppBarLayout = binding.adminLoginAppBarLayout
-
         val toolbar: androidx.appcompat.widget.Toolbar = binding.adminLoginToolbar
 
         toolbar.setNavigationOnClickListener {
@@ -35,7 +31,6 @@ class AdminLogin : Fragment() {
 
         binding.submitButton.setOnClickListener {
 
-            if(binding.adminLoginTv1.text.toString()=="admin" && binding.adminLoginPassword.text.toString()=="admin"))
             findNavController().navigate(R.id.action_adminLogin_to_adminMainPage)
         }
         return binding.root
