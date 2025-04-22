c076edce5bc1dd5d11b4209e01cdac4b629f6823
ishan Parashar <Ishanparashar24@gmail.com>
2025-04-22 18:31:22 +0530

    error fixes part 4

--- app/src/main/java/com/example/a_connect/login/AdminLogin.kt
+++ app/src/main/java/com/example/a_connect/login/AdminLogin.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.a_connect.R
@@ -31,7 +32,18 @@ class AdminLogin : Fragment() {
 
         binding.submitButton.setOnClickListener {
 
-            findNavController().navigate(R.id.action_adminLogin_to_adminMainPage)
+            if (binding.adminLoginEmail.text.toString() == "admin@gmail.Com" && binding.adminLoginPassword.text.toString() == "1234") {
+
+                findNavController().navigate(R.id.action_adminLogin_to_adminMainPage)
+
+            }
+
+            if ( binding.adminLoginPassword.text.toString() != "1234") {
+                Toast.makeText(context, "wrong password please try again !!", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.adminLoginEmail.text.toString() != "admin@gmail.Com") {
+                Toast.makeText(context, "wrong credentials please try again !!", Toast.LENGTH_SHORT).show()
+            }
         }
         return binding.root
     }
